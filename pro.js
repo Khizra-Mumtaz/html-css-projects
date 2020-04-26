9e3900beb56647ffd7ef2df0b0b28042263752ad
Khizra-Mumtaz <khizramumtaz08@gmail.com>
2020-04-27 00:09:32 +0500

    Now Ready to commit

---
+++ pro.js
@@ -0,0 +1,20 @@
+// const checkname=function(){
+//     let val=document.getElementById('khiz').value
+//     let name1='khizra'
+//     if(val==name1)
+//     { console.log('loged in');
+//     }
+//     else{
+//         console.log('not log in')
+//         }}
+
+// document.querySelector('.one').addEventListener('submit',(event)=>{
+//     event.preventDefault()
+//     console.log(event.target.user.value);
+//     event.target.user.value=''
+    
+// })
+// var h=document.createElement('h1')
+// var c= document.createTextNode('hello world!!!')
+// h.appendChild(c)
+// console.log(h);
